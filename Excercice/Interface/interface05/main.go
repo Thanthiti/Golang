bdf7c046d360ddcbf2fc2e8e20b05c6f5863e933
Thanthiti <145197437+Thanthiti@users.noreply.github.com>
2025-06-08 22:38:41 +0700

    Update Example/interface

--- Excercice/Interface/interface05/main.go
+++ Excercice/Interface/interface05/main.go
@@ -1,1 +1,59 @@
 package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Shaper interface {
+	Area() float64
+	Perimeter() float64
+}
+type Shape3D interface {
+	Shaper
+	Volume() float64
+}
+
+type Rectangle struct {
+	width, heigth float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.heigth
+}
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.width + r.heigth)
+}
+
+type Cube struct {
+	length float64
+}
+
+func (c Cube) Area() float64 {
+	return 6 * (c.length * c.length)
+}
+func (c Cube) Perimeter() float64 {
+	return 12 * c.length
+}
+func (c Cube) Volume() float64 {
+	return math.Pow(c.length,3)
+}
+
+func PrintShape(s Shaper){
+	fmt.Printf("Area: %.2f\n", s.Area())
+    fmt.Printf("Perimeter: %.2f\n", s.Perimeter())
+}
+func PrintCube(s3d Shape3D){
+	fmt.Printf("Area: %.2f\n", s3d.Area())
+    fmt.Printf("Perimeter: %.2f\n", s3d.Perimeter())
+    fmt.Printf("Volume: %.2f\n", s3d.Volume())
+}
+
+func main() {
+	r := Rectangle{5,3}
+	c := Cube{4}
+	fmt.Println("Rectangle")
+	PrintShape(r)
+	fmt.Println("Cube")
+	PrintCube(c)
+}
--- Excercice/Interface/interface06/main.go
+++ Excercice/Interface/interface06/main.go
@@ -1,1 +1,45 @@
 package main
+
+import "fmt"
+
+type Payer interface {
+	Pay(amount float64) string
+}
+type Refunder interface {
+	Refund(amount float64) string
+}
+type CreditCard struct {
+	cardNumber string
+}
+
+func (c CreditCard) Pay(amount float64) string {
+	return fmt.Sprintf("Paid %.2f via CreditCard %s", amount, c.cardNumber)
+}
+
+func (c CreditCard) Refund(amount float64) string {
+	return fmt.Sprintf("Refunded %.2f to CreditCard %s", amount, c.cardNumber)
+}
+
+type Cash struct{}
+
+func (c Cash) Pay(amount float64) string {
+    return fmt.Sprintf("Paid %.2f via Cash", amount)
+}
+
+func ProcessPayment(p Payer, amount float64) {
+    fmt.Println(p.Pay(amount))
+}
+
+func ProcessRefund(r Refunder, amount float64) {
+    fmt.Println(r.Refund(amount))
+}
+
+func main() {
+	cc := CreditCard{cardNumber: "1234-5678-9012-3456"}
+    cash := Cash{}
+
+    ProcessPayment(cc, 1000)
+    ProcessRefund(cc, 500)
+	fmt.Println("------------")
+    ProcessPayment(cash, 200)
+}
