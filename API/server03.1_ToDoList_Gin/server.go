7d0c9eb2d3bf3791aec80a9f4af07459e99d04cb
Thanthiti <145197437+Thanthiti@users.noreply.github.com>
2025-06-29 23:54:47 +0700

    API update

--- API/server03.1_ToDoList_Gin/server.go
+++ API/server03.1_ToDoList_Gin/server.go
@@ -1,18 +1,175 @@
 package main
 
-
 import (
+	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
-type s struct{
-	a float64
+// Model
+type Todo struct {
+	ID   int    `json:"id"`
+	Task string `json:"task"`
 }
+
+// Logger
+var logger = logrus.New() 
+
+// Error Response
+
+type ErrorResponse struct{
+	Error ErrorDetail `json:"error"`
+}
+type ErrorDetail struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+func NewErrorResponse(code,message string) ErrorResponse{
+	return ErrorResponse{
+		Error:ErrorDetail{
+			Code: code,
+			Message:message,
+		},
+	}
+}
+
+//Controller 
+type TodoController struct{
+	todos []Todo
+	nextID int
+}
+
+// Constructor FN
+func NewTodoController() *TodoController{
+	return &TodoController{
+		todos: []Todo{},
+		nextID: 1,
+	}
+}
+
+func (c *TodoController) AddTodo(ctx *gin.Context){
+	var t []Todo
+	if err := ctx.ShouldBindJSON(&t); err != nil{
+		logger.WithError(err).Error("Invalid JSON")
+		ctx.JSON(http.StatusBadRequest,NewErrorResponse("ERR_INVALID_ID","Invalid ID"))
+		return
+	}
+	
+	for i := range t{
+		if strings.TrimSpace(t[i].Task) == ""{
+			logger.Warn("Task is required")
+			ctx.JSON(http.StatusBadRequest,NewErrorResponse("ERR_TASK_REQUIRED","Task is required"))
+			return
+		}
+		t[i].ID = c.nextID
+		c.nextID++
+		c.todos = append(c.todos, t[i])
+
+		logger.WithFields(logrus.Fields{
+		"id":   t[i].ID,
+		"task": t[i].Task,
+		}).Info("Added new Todo")
+
+	}
+	ctx.JSON(http.StatusOK,t)
+}
+
+func (c *TodoController) ListTodo(ctx *gin.Context){
+	logger.Info("Listing all todos")
+	ctx.JSON(http.StatusOK,c.todos)
+}
+
+func (c *TodoController) UpdateTodo(ctx *gin.Context) {
+	// Get id of http and convert to int
+    idStr := ctx.Param("id")
+    id, err := strconv.Atoi(idStr)
+    if err != nil || id <= 0 {
+		logger.WithField("idParam",idStr).Error("Invalid ID")
+        ctx.JSON(http.StatusBadRequest,NewErrorResponse("ERR_INVALID_ID", "Invalid ID"))
+        return
+    }
+	
+	var updateData Todo
+    if err := ctx.ShouldBindJSON(&updateData); err != nil {
+		logger.WithError(err).Error("Invalid JSON")
+		ctx.JSON(http.StatusBadRequest,NewErrorResponse("ERR_INVALID_JSON", "Invalid JSON"))
+        return
+    }
+	
+	
+    if strings.TrimSpace(updateData.Task) == "" {
+		logger.Warn("Task is required")
+		ctx.JSON(http.StatusBadRequest,NewErrorResponse("ERR_INVALID_REQUIRED", "Task is required"))
+        return
+    }
+
+    updated := false
+    for i, todo := range c.todos {
+        if todo.ID == id {
+            c.todos[i].Task = updateData.Task
+            updated = true
+			logger.WithFields(logrus.Fields{
+				"id":id,
+				"task":updateData.Task,
+			}).Info("Todo updated")
+            ctx.JSON(http.StatusOK, c.todos[i])
+            break
+        }
+    }
+	
+    if !updated {
+		logger.WithField("id", id).Warn("Todo not found")
+		ctx.JSON(http.StatusNotFound, NewErrorResponse("ERR_TODO_NOT_FOUND", "Todo not found"))
+		return
+	}
+}
+
+
+func (c *TodoController) DeleteTodo(ctx *gin.Context){
+	idStr := ctx.Param("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil || id <= 0 {
+		logger.WithField("idParam",idStr).Error("Invalid ID")
+        ctx.JSON(http.StatusBadRequest,NewErrorResponse("ERR_INVALID_ID", "Invalid ID"))        
+		return
+	}
+
+	found := false
+	for i, data := range c.todos {
+		if data.ID == id {
+			c.todos = append(c.todos[:i], c.todos[i+1:]...)
+			found = true
+
+			logger.WithField("id",id).Info("Todo Deleted")
+			break
+		}
+	}
+
+	if !found {
+		logger.WithField("id", id).Warn("Todo not found")
+		ctx.JSON(http.StatusNotFound, NewErrorResponse("ERR_TODO_NOT_FOUND", "Todo not found"))
+		return
+	}
+
+	ctx.Status(http.StatusNoContent)
+}
+
 
 func main(){
-	r := gin.Default()
+	logger.SetFormatter(&logrus.JSONFormatter{})
+	logger.SetLevel(logrus.InfoLevel)
 
+	r := gin.Default()
+	ctrl := NewTodoController()
+
+	r.GET("/todo/",ctrl.ListTodo)
+	r.POST("/todo/",ctrl.AddTodo)
+	r.PUT("/todo/edit/:id",ctrl.UpdateTodo)
+	r.DELETE("/todo/delete/:id",ctrl.DeleteTodo)
 	r.Run(":8080")
-	
 }
+
--- API/server03_ToDoList_Original/server.go
+++ API/server03_ToDoList_Original/server.go
@@ -6,53 +6,112 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+
 )
 
+// Model
 	type Todo struct {
 		ID   int    `json:"id"`
 		Task string `json:"task"`
 	}
+	//Controller 
+	type TodoController struct{
+		todos []Todo
+		nextID int
+	}
 
-	var todos  = []Todo{}
-	var nextID = 1
-
-	func addTodo(w http.ResponseWriter, r *http.Request) {
-		var t Todo
-		json.NewDecoder(r.Body).Decode(&t)
-		fmt.Println("Do Here")
-		t.ID = nextID
-		nextID++
-		todos = append(todos, t)
-		json.NewEncoder(w).Encode(t)
-		
+	// Constructor FN
+	func NewTodoController() *TodoController{
+		return &TodoController{
+			todos: []Todo{},
+			nextID: 1,
+		}
 	}
 	
-	func listTodos(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Show To do list")
-		json.NewEncoder(w).Encode(todos)
+	// Method Post
+	func (c *TodoController) addTodo(w http.ResponseWriter, r *http.Request) {
+		var t Todo
+
+		if err := json.NewDecoder(r.Body).Decode(&t);err != nil{
+			http.Error(w,`{"Error" : "Invalid Json"}`,http.StatusBadRequest)
+			return
+		}
+		if strings.TrimSpace(t.Task)== ""{
+			http.Error(w,`{"Error" : "Task is required"}`,http.StatusBadRequest)
+			return
+		}
+
+		t.ID = c.nextID 	
+		c.nextID++		
+		c.todos = append(c.todos, t)
+		
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(t)
+	}
+	
+	// Method Get
+	func (c *TodoController) listTodos(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(c.todos)
 	}
 
-	func deleteTodo(w http.ResponseWriter, r *http.Request) {
-		idStr := strings.TrimPrefix(r.URL.Path, "/todo/")
-		id, _ := strconv.Atoi(idStr)
-		for i, t := range todos {
-			if t.ID == id {
-				todos = append(todos[:i], todos[i+1:]...)
+	// Method Put
+	func (c *TodoController) UpdateTodos(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		idstr := strings.TrimPrefix(r.URL.Path,"/todo/edit/")
+		id,err  := strconv.Atoi(idstr)
+
+		if err != nil {
+			http.Error(w,`{"error" : "invalid ID (PUT)"}`,http.StatusBadRequest)
+			return
+		}
+		for _,data := range c.todos{
+			if id == data.ID{
+				json.NewEncoder(w).Encode(data)
 				break
 			}
 		}
+	}
+	
+	// Method Delete
+	func (c *TodoController) deleteTodo(w http.ResponseWriter, r *http.Request) {
+
+		idStr := strings.TrimPrefix(r.URL.Path, "/todo/")
+		id, err := strconv.Atoi(idStr)
+
+		if err != nil || id <= 0{
+			http.Error(w,`{"error" : "invalid ID"}`,http.StatusBadRequest)
+		}
+		
+		found := false
+		for i, data := range c.todos {
+			if data.ID == id {
+				c.todos = append(c.todos[:i], c.todos[i+1:]...)
+				found = true
+				break
+			}
+		}
+		if !found{
+			http.Error(w,`{"error" : "Todo not found"}`,http.StatusBadRequest)
+			return
+		}
+
 		w.WriteHeader(http.StatusNoContent)
 	}
 
 	func main() {
+		ctrl := NewTodoController()
+		fmt.Println(ctrl.nextID)
 		http.HandleFunc("/todo", func(w http.ResponseWriter, r *http.Request) {
 			switch r.Method {
-			case "GET":
-				listTodos(w, r)
-			case "POST":
-				addTodo(w, r)
+			case http.MethodGet:
+				ctrl.listTodos(w, r)
+			case http.MethodPost:
+				ctrl.addTodo(w, r)
 			}
 		})
-		http.HandleFunc("/todo/", deleteTodo)
+		http.HandleFunc("/todo/", ctrl.deleteTodo)
+		http.HandleFunc("/todo/edit/", ctrl.UpdateTodos)
 		http.ListenAndServe(":8080", nil)
 	}
